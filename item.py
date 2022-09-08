2e81c96103d2bd57c6876de52563956794a1cfc8
Cecilia7337 <sunrui7337@gmail.com>
2022-09-07 21:34:03 -0700

    added Owner

--- item.py
+++ item.py
@@ -4,16 +4,16 @@ from flask_restful import Resource, Api
 from flask_jwt import JWT, jwt_required, current_identity
 from marshmallow import Schema, fields
 
-items = [{'category': 'O1', 'name': 'T1', 'quantity': 2}, {'category': 'O2', 'name': 'T2', 'quantity': 10},
-         {'category': 'O3', 'name': 'T3', 'quantity': 8}, {'category': 'O4', 'name': 'T4', 'quantity': 2},
-         {'category': 'O5', 'name': 'T5', 'quantity': 2}]
+items = [{'category': 'O1', 'name': 'T1', 'quantity': 2, 'owner': 'Joe'}, {'category': 'O2', 'name': 'T2', 'quantity': 10, 'owner': 'Mike'},
+         {'category': 'O3', 'name': 'T3', 'quantity': 8, 'owner': 'Joe'}, {'category': 'O4', 'name': 'T4', 'quantity': 6, 'owner': 'Joe'},
+         {'category': 'O5', 'name': 'T5', 'quantity': 2, 'owner': 'Mike'}]
 
 
 class BarQuerySchema(Schema):
     category = fields.Str()
     name = fields.Str(required=True)
     quantity = fields.Int()
-
+    owner = fields.Str()
 
 schema = BarQuerySchema()
 
@@ -36,8 +36,9 @@ class Item(Resource):
 
         category = args.get('category',None)
         quantity = args.get('quantity',None)
+        owner = args.get('owner', None)
 
-        item = {'name': name, 'quantity': quantity, 'category': category}
+        item = {'name': name, 'quantity': quantity, 'category': category, 'owner': owner}
         items.append(item)
         return {'item': next(filter(lambda x: x['name'] == name, items), None)}
 
@@ -53,14 +54,17 @@ class Item(Resource):
     def put(self):
         args = request.args
         name = args['name']
-        category = args['category']
-        quantity = args['quantity']
+        category = args.get('category', None)
+        quantity = args.get('quantity', None)
+        owner = args.get('owner', None)
+
         # Once again, print something not in the args to verify everything works
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
-            item = {'name': name, 'quantity': quantity, 'category': category}
+            item = {'name': name, 'quantity': quantity, 'category': category, 'owner': owner}
             items.append(item)
         else:
             item['quantity'] = quantity
             item['category'] = category
+            item['owner'] = owner
         return item
